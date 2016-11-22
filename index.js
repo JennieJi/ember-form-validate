986dbdcfec1e2013b86ca3ac0e497334092abdc2
Jennie Ji <jennie.ji@shopeemobile.com>
2016-11-22 19:41:22 +0800

    Add blueprint for bower dependency

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-form-validate'
+  name: 'ember-form-validate',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import(app.bowerDirectory + '/light-validate-js/dist/Validator.validate.js');
+    app.import(app.bowerDirectory + '/light-validate-js/dist/Validator.validator.js');
+  }
 };
