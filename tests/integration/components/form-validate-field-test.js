2a5cec3dcf9a4fd4e30e8fc71a41847d0d674abf
Jennie Ji <jennie.ji@shopeemobile.com>
2017-03-15 18:46:43 +0800

    #9 add test cases

--- tests/integration/components/form-validate-field-test.js
+++ tests/integration/components/form-validate-field-test.js
@@ -1,5 +1,14 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+import {ValidateGroup, createValidator} from 'ember-form-validate/services/validator';
+
+const validatorWithErrorMessage = createValidator(() => 'Error Message');
+// const validatorPass = createValidator(function() { return true; });
+
+function getGroupInstance() {
+  return new ValidateGroup();
+}
 
 moduleForComponent('form-validate-field', 'Integration | Component | form validate field', {
   integration: true
@@ -22,4 +31,85 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+
+  this.render(hbs`
+    {{#form-validate-field as |validate errorMessage|}}
+      {{errorMessage}}
+    {{/form-validate-field}}
+  `);
+
+  assert.equal(this.$().text().trim(), '');
+});
+
+test('register a validator by setting "validatorGroup" property', function(assert) {
+  const group = getGroupInstance();
+  this.set('validatorGroup', group);
+  this.render(hbs`
+    {{form-validate-field
+    validatorGroup=validatorGroup}}
+  `);
+  assert.equal(group.fields.length, 1);
+});
+
+test('set "disabled" and it should be register/unregistered from validatorGroup', function(assert) {
+  const group = getGroupInstance();
+  this.set('validatorGroup', group);
+  this.set('diabled', false);
+  this.render(hbs`
+      {{form-validate-field
+      disabled=disabled
+      validatorGroup=validatorGroup}}
+  `);
+  assert.equal(group.fields.length, 1);
+  this.set('disabled', true);
+  assert.equal(group.fields.length, 0);
+  this.set('disabled', false);
+  assert.equal(group.fields.length, 1);
+});
+test('destory/init component and it should be register/unregistered from validatorGroup', function(assert) {
+  const group = getGroupInstance();
+  this.set('validatorGroup', group);
+  this.set('showComponent', true);
+
+  this.render(hbs`
+    {{#if showComponent}}
+      {{form-validate-field
+      disabled=disabled
+      validatorGroup=validatorGroup}}
+    {{/if}}
+  `);
+  assert.equal(group.fields.length, 1);
+  this.set('showComponent', false);
+  assert.equal(group.fields.length, 0);
+  this.set('showComponent', true);
+  assert.equal(group.fields.length, 1);
+});
+test('component validate failed, it should have error message', function(assert) {
+  const group = getGroupInstance();
+  this.set('validators', validatorWithErrorMessage);
+  this.set('validatorGroup', group);
+  this.render(hbs`
+    {{#form-validate-field
+      validatorGroup=validatorGroup
+      validators=validators
+      as |validate errorMessage|}}
+      <div class='error-message'>{{errorMessage}}</div>
+      <div class='validate-button' {{action validate}}>Validate</div>
+    {{/form-validate-field}}
+  `);
+  let $errorMessage = this.$('.error-message');
+  let component = group.fields[0];
+  assert.equal(component.get('_validators').length, 1);
+  this.$('.validate-button').click();
+  return wait().then(() => {
+    assert.equal($errorMessage.text(), 'Error Message');
+    component.resetValidate();
+    return wait().then(() => {
+      assert.equal($errorMessage.text(), '');
+      group.validate().catch(e => e);
+      return wait().then(() => {
+        assert.equal($errorMessage.text(), 'Error Message');
+      });
+    });
+  });
 });
